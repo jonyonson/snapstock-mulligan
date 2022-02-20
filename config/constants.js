eb443aeb9152c16d510ea9059f946db810f5fbec
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2022-02-20 06:32:23 -0600

    feat: proxy api requests

--- config/constants.js
+++ config/constants.js
@@ -1,7 +1,11 @@
-export const IEX_CLOUD_API_KEY = process.env.NEXT_PUBLIC_IEX_CLOUD_API_KEY;
+let BASE_API_URL;
 
-export const IEX_API = {
-  SEARCH: 'https://cloud.iexapis.com/stable/search/',
-  TOKEN: `?token=${IEX_CLOUD_API_KEY}`,
-  KEY: IEX_CLOUD_API_KEY,
+if (process.env.NODE_ENV === 'development') {
+  BASE_API_URL = 'http://localhost:5000';
+} else if (process.env.NODE_ENV === 'production') {
+  BASE_API_URL = 'https://snapstock.herokuapp.com';
+}
+
+export const API = {
+  SEARCH: `${BASE_API_URL}/api/stocks/search`,
 };
--- hooks/use-search.js
+++ hooks/use-search.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { IEX_API } from '../config/constants';
+import { API } from '../config/constants';
 
 const cache = {};
 function fetchSuggestions(value) {
@@ -7,10 +7,7 @@ function fetchSuggestions(value) {
     return Promise.resolve(cache[value]);
   }
 
-  // const ALPHA_VANTAGE_API_KEY = process.env.NEXT_PUBLIC_ALPHA_VANTAGE_API_KEY;
-  // const url = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${value}&apikey=${ALPHA_VANTAGE_API_KEY}`;
-
-  return fetch(IEX_API.SEARCH + value + IEX_API.TOKEN)
+  return fetch(`${API.SEARCH}/${value}`)
     .then((res) => res.json())
     .then((result) => {
       cache[value] = result;
@@ -20,7 +17,6 @@ function fetchSuggestions(value) {
 
 export default function useSearch(searchTerm) {
   const [suggestions, setSuggestions] = useState([]);
-  // const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     if (searchTerm.trim() !== '') {
