1dd6c54fc4bf4b6fbb80d26ca4a2c6930f4a63ca
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2022-02-19 19:17:32 -0600

    test: testing setup/config

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,15 @@ import Search from '../components/Search/Search';
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div>
       <Head>
         <title>Snapstock Mulligan</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <Search />
+      <h1>Snapstock</h1>
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <Search />
+      </div>
     </div>
   );
 }
